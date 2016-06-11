afa49a14aefc8752770b8223900515588777c6ee
Zhang Cheng <13501393281@163.com>
2016-06-11 19:27:01 -0400

    renew setup script to automatically install all the extension and then quit

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,9 @@
-﻿Write-Host 'begin to clone the repo into local' -ForegroundColor Green
-git clone git@github.com:chantisnake/vim_config.git .\vimfiles
+﻿$location = Get-Location
+
+Set-Location $HOME
+Write-Host 'begin to clone the repo into local' -ForegroundColor Green
+git clone git@github.com:chantisnake/vim-config.git .\vimfiles
+git clone https://github.com/VundleVim/Vundle.vim.git .\vimfiles\bundle\Vundle.vim 
 Write-Host 'git clone finished, begin writing .ideavimrc' -ForegroundColor Green
 $stream = [System.IO.StreamWriter] ".ideavimrc"
 $stream.Write('source ~/vimfiles/idea_vim_config.vim')
@@ -12,3 +16,6 @@ Write-Host 'vimrc write finished, begin writing vsvimrc' -ForegroundColor Green
 $stream = [System.IO.StreamWriter] "_vsvimrc"
 $stream.Write('source ~/vimfiles/vs_vim_config.vim')
 $stream.Close()
+Invoke-Expression 'vim +PluginInstall +qa'
+
+Set-Location $location
